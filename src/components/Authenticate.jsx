709da2c46dcc67c1757418d9ec24d0ac1d4b8733
Sandough <jose.sndov@gmail.com>
2024-08-09 06:28:37 -0500

    Added the form, finally fixed git

---
+++ src/components/Authenticate.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Authenticate = ({ token }) => {
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const handleClick = async () => {
+    try {
+      // console.log(token)
+      const newCall = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const responseCall = await newCall.json();
+      setSuccessMessage(responseCall.message);
+      // console.log(successMessage);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <>
+      <h2>Authenticate</h2>
+      {successMessage && <p>{successMessage}</p>}
+
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token</button>
+    </>
+  );
+};
+
+export default Authenticate;
